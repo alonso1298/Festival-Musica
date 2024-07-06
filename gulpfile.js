0f9266bd0570322e14affc6a4f8c7d61733f48ab
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-07-06 17:59:33 -0600

    Compilar sass con gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
-export function hola( done ) { // done le avisa a gulp que la funcion ah acabado
-    console.log('Hola desde Gulpfile.js');
+import { src, dest } from 'gulp'
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+
+const sass = gulpSass(dartSass)
+
+export function css(done){
+    src('src/scss/app.scss')
+        .pipe( sass() )
+        .pipe( dest('build/css') )
 
     done()
 }
