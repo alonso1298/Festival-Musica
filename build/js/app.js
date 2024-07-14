cf7d2d61f75c12ad7bb7dfd5df7a6f549b207187
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-07-14 14:33:01 -0600

    Se crea galeria con JS

--- build/js/app.js
+++ build/js/app.js
@@ -1,1 +1,17 @@
-alert('Hola Mundo')
+document.addEventListener('DOMContentLoaded', function(){
+    crearGaleria()
+})
+
+function crearGaleria() {
+
+    const CANTIDAD_IMAGENES = 16
+    const galeria = document.querySelector('.galeria-imagenes')
+
+    for(let i = 1; i <= CANTIDAD_IMAGENES; i++) {
+        const imagen = document.createElement('IMG')
+        imagen.src = `src/img/gallery/full/${i}.jpg`
+        imagen.alt = 'Imagen Galeria'
+        
+        galeria.appendChild(imagen)
+    }
+}
