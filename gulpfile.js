097c0af869dca9cc34ac38cfcaf0b382951e3bf0
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-07-13 18:32:39 -0600

    Se incorporta js al proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
-import { src, dest, watch } from 'gulp'
+import { src, dest, watch, series } from 'gulp' //Series permite ejecutar una tarea y despues otra
 import * as dartSass from 'sass'
 import gulpSass from 'gulp-sass'
 
 const sass = gulpSass(dartSass)
+
+export function js(done){
+    src('src/js/app.js')
+        .pipe( dest('build/js') )
+    done()
+}
 
 export function css(done){
     src('src/scss/app.scss', {sourcemaps: true}) //sourcemaps podemos saber en que archivo de sass se encuentra el codigo para modificarlo
@@ -14,4 +20,7 @@ export function css(done){
 
 export function dev(){
     watch('src/scss/**/*.scss', css)
+    watch('src/js/**/*.js', js)
 }
+
+export default series( js, css, dev);
