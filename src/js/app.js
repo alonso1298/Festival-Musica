cb9d82e7ae14defedbf8c0c34e93e074e879750f
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-07-14 20:11:44 -0600

    se agrega efecto de Scroll a una seccion

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
     navegacionFija()
     crearGaleria()
     resaltarEnlace()
+    scrollNav()
 })
 
 function navegacionFija(){
@@ -92,6 +93,20 @@ function resaltarEnlace(){
             if(link.getAttribute('href') === '#' + actual) {
                 link.classList.add('active')
             }
+        })
+    })
+}
+
+function scrollNav(){
+    const navLinks = document.querySelectorAll('.navegacion-principal a')
+
+    navLinks.forEach( link => {
+        link.addEventListener('click', e => {
+            e.preventDefault()
+            const sectionScroll = e.target.getAttribute('href')
+            const section = document.querySelector(sectionScroll)
+
+            section.scrollIntoView({behavior: 'smooth'})
         })
     })
 }
