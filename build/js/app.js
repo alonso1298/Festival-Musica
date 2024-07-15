b802908d0dbfef6797a58404c8ac9ad1de64d51a
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-07-14 18:48:38 -0600

    Se finaliza el Modal

--- build/js/app.js
+++ build/js/app.js
@@ -30,7 +30,14 @@ function crearGaleria() {
         modal.classList.add('modal')
         modal.onclick = cerrarModal
 
+        // Boton cerrar modal
+        const cerrarModalBtn = document.createElement('BUTTON')
+        cerrarModalBtn.textContent = 'X'
+        cerrarModalBtn.classList.add('btn-cerrar')
+        cerrarModalBtn.onclick = cerrarModal
+
         modal.appendChild(imagen)
+        modal.appendChild(cerrarModalBtn)
 
         // Agregar al HTML
         const body = document.querySelector('body')
