99c9e089efbe33cbcaf58dff71570171d01b6756
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-07-17 22:37:07 -0600

    Se sirven imagenes AVIF y Webp

--- src/js/app.js
+++ src/js/app.js
@@ -24,12 +24,11 @@ function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes')
 
     for(let i = 1; i <= CANTIDAD_IMAGENES; i++) {
-        const imagen = document.createElement('IMG')
-        imagen.loading = 'lazy'
-        imagen.width = '300'
-        imagen.height = '200'
-        imagen.src = `src/img/gallery/thumb/${i}.jpg`
-        imagen.alt = 'Imagen Galeria'
+        const imagen = document.createElement('PICTURE')
+        imagen.innerHTML = `
+            <source srcset="build/img/gallery/thumb/${i}.avif" type="image/avif">
+            <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
+            <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">`;
         
         //Event Handler es el proceso de detectar y responder a la interaccion del usurario
         imagen.onclick = function() {
@@ -41,9 +40,11 @@ function crearGaleria() {
 }
 
 function mostrarImagen(i) {
-    const imagen = document.createElement('IMG')
-    imagen.src = `src/img/gallery/full/${i}.jpg`
-    imagen.alt = 'Imagen Galeria'
+    const imagen = document.createElement('PICTURE')
+    imagen.innerHTML = `
+        <source srcset="build/img/gallery/full/${i}.avif" type="image/avif">
+        <source srcset="build/img/gallery/full/${i}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/gallery/full/${i}.jpg" alt="imagen galeria">`;
 
     // Generar Modal
     const modal = document.createElement('DIV')
